5087d69069cbaef544f2d5812d65b3842b8dccd2
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-09-29 11:33:28 +0100

    Removing additional "export"
    We already have one at the bottom of the file.

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export const ModuleListScreen = () => {
+const ModuleListScreen = () => {
   // Initialisations -------------------------------------------------
   // State -----------------------------------------------------------
   // Handlers --------------------------------------------------------
