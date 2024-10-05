73f6ee726a017d8bc560f01976ef4540ab205c7a
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-10-05 18:25:34 +0100

    Revert "Merge pull request #3 from Angzuk/master"
    This reverts commit 77b1218480fbca101936f8d2924105846d20c658, reversing
    changes made to a641f0d08071c4778c9a8d7686b4ab637e5148a5.

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,46 +1,18 @@
-import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import Screen from '../layout/Screen';
-
-import initialModules from '../../data/module';
 
 const ModuleListScreen = () => {
   // Initialisations -------------------------------------------------
-  const modules = initialModules;
   // State -----------------------------------------------------------
   // Handlers --------------------------------------------------------
-  const handleSelect = () => alert("Item selected");
   // View ------------------------------------------------------------
   return (
     <Screen>
-      <ScrollView style={styles.container}>
-        {
-          modules.map((modules)=>{
-            return(
-              <Pressable key={modules.ModuleCode} onPress={handleSelect}>
-                <View style={styles.item}>
-                  <Text style={styles.text}>
-                    {modules.ModuleCode} {modules.ModuleName}
-                  </Text>
-                </View>
-              </Pressable>
-            )
-          })
-        }
-      </ScrollView>
+      <Text>List</Text>
     </Screen>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {},
-  item: {
-    paddingVertical: 15,
-    borderTopWidth: 1,
-    borderColor: 'lightgray',
-  },
-  text: {
-    fontSize: 16,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default ModuleListScreen;
