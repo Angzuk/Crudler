02971d9002efb222d975f9fa374016f6e35cd6ec
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-10-05 19:38:56 +0100

    Extracting ModuleItem.js
    Refactoring the code: Moving into its own component ModuleItem.js, functionality that is responsible for rendering specific module.

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,5 +1,6 @@
-import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import Screen from '../layout/Screen';
+import ModuleItem from '../entity/modules/ModuleItem';
 
 import initialModules from '../../data/module';
 
@@ -14,15 +15,9 @@ const ModuleListScreen = () => {
     <Screen>
       <ScrollView style={styles.container}>
         {
-          modules.map((modules)=>{
+          modules.map((module)=>{
             return(
-              <Pressable key={modules.ModuleCode} onPress={handleSelect}>
-                <View style={styles.item}>
-                  <Text style={styles.text}>
-                    {modules.ModuleCode} {modules.ModuleName}
-                  </Text>
-                </View>
-              </Pressable>
+              < ModuleItem key={module.ModuleCode} module={module} onSelect={handleSelect}/>
             )
           })
         }
@@ -33,14 +28,6 @@ const ModuleListScreen = () => {
 
 const styles = StyleSheet.create({
   container: {},
-  item: {
-    paddingVertical: 15,
-    borderTopWidth: 1,
-    borderColor: 'lightgray',
-  },
-  text: {
-    fontSize: 16,
-  },
 });
 
 export default ModuleListScreen;
