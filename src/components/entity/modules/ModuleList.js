1f6aea7fa3d27a1fa62937c029933c1ce042ca45
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-10-05 20:13:07 +0100

    Creating a new component ModuleList.js

---
+++ src/components/entity/modules/ModuleList.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const ModuleList = () => {
+// Initialisations ---------------------
+// State -------------------------------
+// Handlers ----------------------------
+// View --------------------------------
+return (
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ModuleList;
