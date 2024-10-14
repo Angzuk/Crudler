6992981529c694d3c0da63f2a5a86272dced5ea3
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-10-14 10:29:28 +0100

    Adding a new (pre-built) module

--- src/components/screens/ModuleAddScreen.js
+++ src/components/screens/ModuleAddScreen.js
@@ -1,14 +1,33 @@
 import { StyleSheet, Text } from 'react-native';
 import Screen from '../layout/Screen';
+import Icons from '../UI/Icons';
+import { Button, ButtonTray } from '../UI/Button';
 
-const ModuleAddScreen = () => {
+const defaultModule = {
+  ModuleID: Math.floor(100000 + Math.random() * 900000),
+  ModuleCode: 'CI6330',
+  ModuleName: 'Mobile Application Development',
+  ModuleLevel: 6,
+  ModuleLeaderID: 1,
+  ModuleLeaderName: 'Graeme JONES',
+  ModuleImage: 'https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg',
+}
+
+const ModuleAddScreen = ({navigation, route}) => {
   // Initialisations -------------------------------------------------
+  const { onAdd } = route.params;
+
   // State -----------------------------------------------------------
   // Handlers --------------------------------------------------------
+  const handleAdd = () => onAdd(defaultModule);
+
   // View ------------------------------------------------------------
   return (
     <Screen>
       <Text>Add</Text>
+      <ButtonTray>
+        <Button label="Add" icon={<Icons.Add/>} onClick={handleAdd} />
+      </ButtonTray>
     </Screen>
   );
 }
